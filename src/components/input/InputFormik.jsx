888dda420e7f2a2ecaa609394f9b09b8cc48b7da
fehuyngo <fe.huyngo@gmail.com>
2023-02-22 11:09:35 +0700

    first commit

---
+++ src/components/input/InputFormik.jsx
@@ -0,0 +1,23 @@
+import { useField } from "formik";
+import React from "react";
+
+const InputFormik = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className="flex flex-col gap-3 mb-5">
+      <label htmlFor={props.id} className="cursor-pointer">
+        {label}
+      </label>
+      <input
+        className="p-4 border border-gray-100 rounded-lg bg-white outline-none transition-all focus:border-blue-500"
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error && (
+        <p className="text-red-500 text-sm">{meta.error}</p>
+      )}
+    </div>
+  );
+};
+
+export default InputFormik;
---
+++ src/components/radio/RadioHook.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useController } from "react-hook-form";
+
+const RadioHook = ({ control, ...props }) => {
+  const { field } = useController({
+    control,
+    name: props.name,
+    defaultValue: props.value,
+  });
+  return (
+    <label className="w-5 h-5 cursor-pointer custom-radio">
+      <input
+        type="radio"
+        {...field}
+        {...props}
+        className="hidden"
+        checked={props.checked}
+      />
+      <div className="bg-white w-full h-full rounded-full"></div>
+    </label>
+  );
+};
+
+export default RadioHook;
